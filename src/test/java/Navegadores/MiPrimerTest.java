db22e352b1c63eab62d770fce65c407459dc9924
Gaba91 <aneugaba@gmail.com>
2023-02-18 14:11:24 -0600

    Primer commit

---
+++ src/test/java/Navegadores/MiPrimerTest.java
@@ -0,0 +1,9 @@
+package Navegadores;
+
+import org.openqa.selenium.WebDriver;
+
+public class MiPrimerTest
+{
+    private WebDriver driver;
+    //Clonar el proyecto en Selenium y subir los cambios que creamos de Selenium (Carpeta de Navegadores)
+}
