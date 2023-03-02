01d0f614d9dff9df46cc7296146183822cdd9e7c
Gaba91 <aneugaba@gmail.com>
2023-03-02 14:07:46 -0600

    Commit Locators

---
+++ src/test/java/Locators/Locators.java
@@ -0,0 +1,97 @@
+package Locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Locators
+{
+    private WebDriver driver;
+
+    @BeforeTest
+    public void Setup () throws InterruptedException {
+        //Se coloca la direccion del driver, segun corresponda (chrome, mozilla, safari, edge, etc...)
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/Drivers/chromedriver.exe");
+
+        //Se crea la instancia u objeto del navegador
+        driver = new ChromeDriver();
+
+        //Maximiza el navegador
+        driver.manage().window().maximize();
+
+        //Enviamos la URL al navegador
+        driver.get("https://demo.guru99.com/test/newtours/");
+        driver.manage().deleteAllCookies();
+        Thread.sleep(3000);
+
+    }
+
+    @Test
+    public void Locators() throws InterruptedException
+    {
+        // ------Localizador por id------
+        /*WebElement demoSite = driver.findElement(By.id("site-name"));
+        Thread.sleep(3000);
+        //click = selecciona / click en el elemento --> "demoSite"
+        demoSite.click();
+        Thread.sleep(3000);*/
+
+        // ------Localizador por nombre------
+        /*WebElement userName = driver.findElement(By.name("userName"));
+        userName.sendKeys("Automation23");
+        Thread.sleep(3000);*/
+
+        // ------Localizador por nombre de clase------
+        /*WebElement logo = driver.findElement(By.className("logo"));
+        logo.click();
+        Thread.sleep(3000);*/
+
+        // ------Localizador por tagname------ <-- se omite por no parecer ser muy funcional
+
+        // ------Localizador por link text------
+        /*WebElement demoSite = driver.findElement(By.linkText("Demo Site"));
+        demoSite.click();
+        Thread.sleep(3000);*/
+
+        // ------Localizador por partial link text------
+        /*WebElement demoSite = driver.findElement(By.partialLinkText("Demo"));
+        demoSite.click();
+        Thread.sleep(3000);*/
+
+        // ------Localizador por cssSelector------
+        //WebElement userName = driver.findElement(By.cssSelector("input[name='usarName']")); <-- opcion #1
+        //WebElement demoSite = driver.findElement(By.cssSelector("div#site-name")); <-- opcion #2
+        //WebElement demoSite = driver.findElement(By.cssSelector("#site-name")); <-- opcion #3 esta tecnica solo funciona por id, no por name
+        //demoSite.click();
+        //WebElement userName = driver.findElement(By.cssSelector("input[type='text'][name='userName']")); <-- opcion #4
+        //userName.sendKeys("Automation");
+        //Thread.sleep(3000);
+
+        // ------Localizador por XPath------
+        //Hay dos tipos de Xpaths: absoluto y relativo
+        //El XPath absoluto por buenas practicas NO debe utilizarse o abusar de el.
+        //Diferencias: el XPath absoluto es toda la ruta/ubicacion del elemento, su sintaxis inicia con una diagonal
+
+        // ------XPath absoluto
+        WebElement  userName = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[2]/td[2]/input"));
+        userName.sendKeys("locura");
+        Thread.sleep(3000);
+
+    }
+
+    @AfterTest
+    public void TearDown()
+    {
+        driver.close();
+        driver.quit();
+
+    }
+
+}
+
+/*Locators: se puede localizar por diferentes atributos, ej. por id, name, class, tagname, link text, partial link text, cssSelector, xpath <-- debe ser siempre la
+ultima tecnica para automatizar, primero agotar todas las demas alternativas antes que usar xpath*/
--- src/test/java/Navegadores/MiPrimerTest.java
+++ src/test/java/Navegadores/MiPrimerTest.java
@@ -1,9 +1,64 @@
 package Navegadores;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class MiPrimerTest
 {
     private WebDriver driver;
-    //Clonar el proyecto en Selenium y subir los cambios que creamos de Selenium (Carpeta de Navegadores)
+
+    @BeforeTest
+    public void Setup () throws InterruptedException {
+        //Se coloca la direccion del driver, segun corresponda (chrome, mozilla, safari, edge, etc...)
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/Drivers/chromedriver.exe");
+
+        //Se crea la instancia u objeto del navegador
+        driver = new ChromeDriver();
+
+        //Maximiza el navegador
+        driver.manage().window().maximize();
+
+        //Enviamos la URL al navegador
+        driver.get("https://demo.guru99.com/test/newtours/");
+        driver.manage().deleteAllCookies();
+        Thread.sleep(3000);
+
+    }
+
+        @Test
+        public void MiPrimerTest() throws InterruptedException
+        {
+
+            WebElement signOn = driver.findElement(By.className("mouseOut"));
+            Thread.sleep(3000);
+            //click = selecciona / click en el elemento --> "signOn"
+            signOn.click();
+            Thread.sleep(2000);
+
+
+        }
+
+        @AfterTest
+        public void TearDown()
+        {
+            //Cierra el navegador
+            driver.close();
+            driver.quit();
+
+        }
+
+
 }
+
+
+/*sendKeys se usa para enviar texto a los inputs
+            inputSearch.sendKeys("collar perros");
+                    Thread.sleep(2000);
+                    //submit = simula el enter
+                    inputSearch.submit();
+                    Thread.sleep(6000);*/
